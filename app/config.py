461cf16db2d94f7fb66f5e1d0f944ed8f60d9c77
metrik420 <metrikcorp@gmail.com>
2024-09-28 01:01:42 -0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,9 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///organizer.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CACHE_TYPE = "simple"
+    CACHE_DEFAULT_TIMEOUT = 300
 
